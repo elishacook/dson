5f043a336835cf52cc4d9ba9746dc7ec5ff45c05
Elisha Cook <elisha@elishacook.com>
2013-12-09 16:13:30 -0500

    Fixed setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'dson',
     version = '0.1.0',
-    modules = ['dson'],
+    py_modules = ['dson'],
     description = 'Like bson, \'cept different.',
     author = 'Elisha Fitch-Cook',
     author_email = 'elisha@elishacook.com',
