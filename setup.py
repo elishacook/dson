fc5ce06f176783245dff08b6d34ccc502b581e7f
Elisha Cook <elisha@elishacook.com>
2013-12-09 16:06:01 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'dson',
+    version = '0.1.0',
+    modules = ['dson'],
+    description = 'Like bson, \'cept different.',
+    author = 'Elisha Fitch-Cook',
+    author_email = 'elisha@elishacook.com',
+    url = 'https://github.com/elishacook/dson',
+    test_suite = 'tests',
+    install_requires=['pytz']
+)
